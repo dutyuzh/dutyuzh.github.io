0c7ebed2f51e12e621a8ff372a81878f7bdc991f
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 12:03:58 -0500

    update configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportTrailingSlash: true,  // Optional: Adds trailing slashes to URLs
 }
 
 module.exports = nextConfig
