28812394ec57efb1e3ddb8683a5e75da62481eff
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 12:10:10 -0500

    Disable Image Optimization for static export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   trailingSlash: true,  // Optional: Adds trailing slashes to URLs
+  images: {
+    unoptimized: true,  // Disables image optimization
+  },
 }
 
 module.exports = nextConfig
