5f5e10e6db1dc2d9b34d312a799911dc6e469aab
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 12:08:38 -0500

    update export files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  exportTrailingSlash: true,  // Optional: Adds trailing slashes to URLs
+  trailingSlash: true,  // Optional: Adds trailing slashes to URLs
 }
 
 module.exports = nextConfig
