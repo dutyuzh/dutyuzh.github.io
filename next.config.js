3004e184deb145b3d9f55e186ff4b8f8ce277cdd
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 13:50:25 -0500

    Fix asset paths for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,  // Disables image optimization
   },
-  basePath: '', // Repository name or base path for the deployment
+  basePath: '/dutyuzh.github.io', // Repository name or base path for the deployment
   assetPrefix: '', // Don't add another prefix here
 }
 
