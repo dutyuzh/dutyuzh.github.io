9d1307e6517eee46edafc228660c9bd43c45684e
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 11:59:57 -0500

    add building configs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // this tells Next.js to generate static files to export
-  images: {
-    unoptimized: true, //disabled the build-in image optimization
-  },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
