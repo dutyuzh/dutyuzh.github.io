05b45fb630a21498b3ba78661de9db4192ada677
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-18 08:57:31 -0500

    set up of gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // this tells Next.js to generate static files to export
+  basePath: '/dutyuzh.github.io'
 }
 
 module.exports = nextConfig
